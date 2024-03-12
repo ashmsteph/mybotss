13587f031e4ec87513b6848373eb135c5a4dc546
Your Name <you.@example.com>
2024-03-11 21:52:45 -0400

    simulate.py

--- simulate.py
+++ simulate.py
@@ -1,7 +1,17 @@
+import pybullet_data
 import pybullet as p
+import time as t
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
-p.disconnect()
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
+    t.sleep(1/60)
+    print(i)
+
+p.disconnect()
