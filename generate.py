c522f640b6284f7aae74938532b5f5f07738e89b
Your Name <you.@example.com>
2024-03-12 23:17:02 -0400

    generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
 import pyrosim.pyrosim as pyrosim 
 
+height = 1
+length = 1
+width = 1
+x = 1
+y = 1
+z = 1
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name = "Box", pos = [0,0,0.5], size = [1,1,1])
+pyrosim.Send_Cube(name = "Box", pos = [x,y,z], size = [height, length, width])
 
 pyrosim.End()
