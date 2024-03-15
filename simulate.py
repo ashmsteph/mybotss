3761437a6820d525486046517ee7f5785645244e
Your Name <you.@example.com>
2024-03-15 13:54:19 -0400

    simulate.py

--- simulate.py
+++ simulate.py
@@ -8,9 +8,14 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
+
+pyrosim.Prepare_To_Simulate(robotId) 
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg") 
+    print(backLegTouch)
     t.sleep(1/60)
     print(i)
 
