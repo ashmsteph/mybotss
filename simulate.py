0e4b05f46dedb49ae91a494fa990f484c84e6527
Your Name <you.@example.com>
2024-03-15 21:17:11 -0400

    simulate.py

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet_data
 import pybullet as p
 import time as t
 import pyrosim.pyrosim as pyrosim
+import numpy as numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,11 +13,16 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId) 
-for i in range(1000):
+backLegSensorValues = numpy.zeros(10000)
+print(backLegSensorValues)
+
+for i in range(10000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg") 
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg") 
+   
     t.sleep(1/60)
     print(i)
-
+    print(backLegSensorValues)
+numpy.save(data, backLegSensorValues)
 p.disconnect()
+# print(backLegSensorValues)
