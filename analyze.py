38c606e529193f6bada3fcf3bddde27fbc755ef8
Your Name <you.@example.com>
2024-03-17 16:52:03 -0400

    analyze.py

--- analyze.py
+++ analyze.py
@@ -1,9 +1,16 @@
-import matplotlib.pyplot 
-import numpy as numpy
+
+import numpy as np
+import matplotlib.pyplot
 
 
-backLegSensorValues = numpy.load("data/file.npy") 
+backLegSensorValues = np.load("data/file.npy")
+frontLegSensorValues = np.load("record/data.npy")
 print(backLegSensorValues)
-
-matplotlib.pyplot.plot(backLegSensorValues)
+print(frontLegSensorValues)
+targetAngles = np.sin((0, np.pi * 2))
+matplotlib.pyplot.plot(targetAngles)
 matplotlib.pyplot.show()
+matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.show()
+
