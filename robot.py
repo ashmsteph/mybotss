4b12100d45f52e0c0808eff5f0fd85b099b52c85
Your Name <you.@example.com>
2024-03-18 12:58:33 -0400

    robot.py

--- robot.py
+++ robot.py
@@ -2,6 +2,7 @@ import pybullet_data
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import time as t
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 class ROBOT:
     def __init__(self):
         # physicsClient = p.connect(p.GUI)
@@ -9,6 +10,7 @@ class ROBOT:
         # self.sensors = {}
         self.motors = {}
         self.robotId = p.loadURDF("body.urdf")
+        self.nn = NEURAL_NETWORK("brain.nndf")
     def Prepare_To_Sense(self):
         self.sensors = {}
         for linkName in pyrosim.linkNametoIndices:
@@ -16,7 +18,13 @@ class ROBOT:
     def Sense(self):
         for i in self.sensors():
             self.sensors.Get_Value() 
-    def Act():
-        pass
+    def Think(self):
+        self.nn.Print()
+        self.nn.Update()
+        print(self.nn)
+    def Act(self):
+        for neuronName in self.nn.Get_Neuron_Names():
+            print(neuronName)
+    
         
             
