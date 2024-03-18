1f781856d1a3b40ca38e4c05ae71c9936e836fb7
Your Name <you.@example.com>
2024-03-18 13:00:00 -0400

    generate.py

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ def Create_World():
     pyrosim.Send_Cube(name = "Box", pos = [x,y,z], size = [height, length, width])
     pyrosim.End()
 
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name = "BackLeg", pos = [0.5, 0, 1.0], size = [height, length, width])
     pyrosim.Send_Joint(name = "Torso_BackLeg", parent = "Torso", child = "BackLeg", 
@@ -23,8 +23,22 @@ def Create_Robot():
     pyrosim.Send_Cube(name = "FrontLeg", pos = [1.0, 0, 0.5], size = [height, length, width])
     pyrosim.End()
 
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0, linkName = "Torso")
+    pyrosim.Send_Motor_Neuron(name = 3, jointName = "Torso_BeckLeg")
+    # pyrosim.Send_Cube(name = "BackLeg", pos = [0.5, 0, 1.0], size = [height, length, width])
+    # pyrosim.Send_Joint(name = "Torso_BackLeg", parent = "Torso", child = "BackLeg", 
+    #                    type = "revolute", position = [1.5, 0, 1.5])
+    # pyrosim.Send_Cube(name = "Torso", pos = [1.0, 0, 1.5], size = [height, length, width])
+    # pyrosim.Send_Joint(name = "Torso_FrontLeg", parent = "Torso", child = "FrontLeg",
+    #                      type = "revolute", position = [1.0, 0, 0])
+    # pyrosim.Send_Cube(name = "FrontLeg", pos = [1.0, 0, 0.5], size = [height, length, width])
+    pyrosim.End()
+
 Create_World()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
 # pyrosim.Send_Cube(name = "Box2", pos = [x,y,z], size = [height, length, width])
 
 # for i in range(10):
