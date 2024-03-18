4254511832d2beb69193fb7da8bc45b0c4ffd9f4
Your Name <you.@example.com>
2024-03-18 05:40:32 -0400

    sensor.py

--- sensor.py
+++ sensor.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pyrosim.pyrosim as pyrosim
 class SENSOR:
-    def __init__(self):
-        pass
+    def __init__(self,linkName,values):
+        self.sensors[self.linkName] = SENSOR(self.linkName)
+        self.values = np.zeros(10000)
+    def Get_Value():
+        backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")    
+        
