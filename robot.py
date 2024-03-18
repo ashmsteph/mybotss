eef410a8cb16a9dfd04f8bb387d83653e88fcef4
Your Name <you.@example.com>
2024-03-18 05:40:56 -0400

    robot.py

--- robot.py
+++ robot.py
@@ -1,3 +1,22 @@
+import pybullet_data 
+import pybullet as p
+import pyrosim.pyrosim as pyrosim
+import time as t
 class ROBOT:
     def __init__(self):
+        # physicsClient = p.connect(p.GUI)
+        # p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        # self.sensors = {}
+        self.motors = {}
+        self.robotId = p.loadURDF("body.urdf")
+    def Prepare_To_Sense(self):
+        self.sensors = {}
+        for linkName in pyrosim.linkNametoIndices:
+            print(linkName)
+    def Sense(self):
+        for i in self.sensors():
+            self.sensors.Get_Value() 
+    def Act():
         pass
+        
+            
